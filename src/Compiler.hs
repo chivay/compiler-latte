975ce4f354070de57c627b6310decb0e1b495742
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2018-12-01 22:14:34 +0100

    Reformat

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -1,14 +1,13 @@
 -- {-# LANGUAGE OverloadedStrings #-}
-
 module Compiler where
 
 import           Control.Monad.Except
 import           Control.Monad.State
-import qualified Data.Text                     as T
+import qualified Data.Text            as T
 
 type CompilerM = ExceptT String (StateT CompilerState IO)
 
-data CompilerState = CompilerState {
-    _filename :: FilePath,
-    _src :: T.Text
-} deriving (Eq, Show)
+data CompilerState = CompilerState
+  { _filename :: FilePath
+  , _src      :: T.Text
+  } deriving (Eq, Show)
--- src/Parse.hs
+++ src/Parse.hs
@@ -1,64 +1,76 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module Parse where
 
 import           Control.Monad
+import qualified Data.Text                              as T
 import           Text.ParserCombinators.Parsec
 import           Text.ParserCombinators.Parsec.Expr
 import           Text.ParserCombinators.Parsec.Language
-import qualified Text.ParserCombinators.Parsec.Token
-                                               as Token
-import qualified Data.Text                     as T
+import qualified Text.ParserCombinators.Parsec.Token    as Token
 
 import           Abs
 
-languageDef = emptyDef
-    { Token.commentStart    = "/*"
-    , Token.commentEnd      = "*/"
-    , Token.commentLine     = "//"
-    , Token.identStart      = letter
-    , Token.identLetter     = alphaNum <|> oneOf ['_']
-    , Token.reservedNames   = [ "if"
-                              , "else"
-                              , "while"
-                              , "class"
-                              , "string"
-                              , "int"
-                              , "boolean"
-                              , "void"
-                              , "return"
-                              ]
-    , Token.reservedOpNames = [ "+"
-                              , "-"
-                              , "*"
-                              , "/"
-                              , "%"
-                              , "<"
-                              , ">"
-                              , "<="
-                              , ">="
-                              , "=="
-                              , "!="
-                              , "!"
-                              , "&&"
-                              , "||"
-                              , ","
-                              ]
+languageDef =
+  emptyDef
+    { Token.commentStart = "/*"
+    , Token.commentEnd = "*/"
+    , Token.commentLine = "//"
+    , Token.identStart = letter
+    , Token.identLetter = alphaNum <|> oneOf ['_']
+    , Token.reservedNames =
+        [ "if"
+        , "else"
+        , "while"
+        , "class"
+        , "string"
+        , "int"
+        , "boolean"
+        , "void"
+        , "return"
+        ]
+    , Token.reservedOpNames =
+        [ "+"
+        , "-"
+        , "*"
+        , "/"
+        , "%"
+        , "<"
+        , ">"
+        , "<="
+        , ">="
+        , "=="
+        , "!="
+        , "!"
+        , "&&"
+        , "||"
+        , ","
+        ]
     , Token.caseSensitive = True
     }
 
 lexer = Token.makeTokenParser languageDef
 
 identifier = Token.identifier lexer
+
 reserved = Token.reserved lexer
+
 reservedOp = Token.reservedOp lexer
+
 parens = Token.parens lexer
+
 braces = Token.braces lexer
 
 integer = Token.integer lexer
+
 semi = Token.semi lexer
+
 whitespace = Token.whiteSpace lexer
+
 commaSep = Token.commaSep lexer
+
 semiSep = Token.semiSep lexer
+
 stringLiteral = Token.stringLiteral lexer
 
 ident :: Parser T.Text
@@ -73,118 +85,121 @@ typ = typInteger <|> typString <|> typBool <|> typVoid
     typVoid = reserved "void" >> return TBool
 
 stmt :: Parser Stmt
-stmt   =
-    blockStmt
-    <|> ifStmt
-    <|> ifElseStmt
-    <|> whileStmt
-    <|> (do reserved "return"; e <- optionMaybe expr; semi; return $ Ret e)
-    <|> (do s <- assStmt; whitespace; semi; return s)
-    <|> (do s <- exprStmt; whitespace; semi; return s)
-    <|> (semi >> return Empty)
+stmt =
+  blockStmt <|> ifStmt <|> ifElseStmt <|> whileStmt <|>
+  (do reserved "return"
+      e <- optionMaybe expr
+      semi
+      return $ Ret e) <|>
+  (do s <- assStmt
+      whitespace
+      semi
+      return s) <|>
+  (do s <- exprStmt
+      whitespace
+      semi
+      return s) <|>
+  (semi >> return Empty)
 
 exprStmt :: Parser Stmt
 exprStmt = ExpS <$> expr
 
 blockStmt :: Parser Stmt
 blockStmt = do
-    stmts <- braces (many stmt)
-    return $ Block stmts
+  stmts <- braces (many stmt)
+  return $ Block stmts
 
 expr :: Parser Expr
 expr = buildExpressionParser operators terms
   where
     operators =
-        [ [Prefix (reservedOp "-" >> return Neg)]
-        , [ Infix (reservedOp "*" >> return (Mul Times)) AssocLeft
-          , Infix (reservedOp "/" >> return (Mul Div)) AssocLeft
-          , Infix (reservedOp "%" >> return (Mul Mod)) AssocLeft
-          ]
-        , [ Infix (reservedOp "+" >> return (Add Plus)) AssocLeft
-          , Infix (reservedOp "-" >> return (Add Minus)) AssocLeft
-          ]
-        , [Prefix (reservedOp "!" >> return Not)]
-        , [ Infix (reservedOp "&&" >> return And) AssocLeft
-          , Infix (reservedOp "||" >> return Or)  AssocLeft
-          ]
-        , [ Infix (reservedOp "<" >> return (Comp Less))          AssocNone
-          , Infix (reservedOp "<=" >> return (Comp LessEqual))    AssocNone
-          , Infix (reservedOp ">" >> return (Comp Greater))       AssocNone
-          , Infix (reservedOp ">=" >> return (Comp GreaterEqual)) AssocNone
-          , Infix (reservedOp "==" >> return (Comp Equal))        AssocNone
-          , Infix (reservedOp "!=" >> return (Comp NEqual))       AssocNone
-          ]
+      [ [Prefix (reservedOp "-" >> return Neg)]
+      , [ Infix (reservedOp "*" >> return (Mul Times)) AssocLeft
+        , Infix (reservedOp "/" >> return (Mul Div)) AssocLeft
+        , Infix (reservedOp "%" >> return (Mul Mod)) AssocLeft
         ]
-    terms = 
-        parens expr
-            <|> try functionCall
-            <|> fmap LitInt         integer
-            <|> fmap (LitString . T.pack) stringLiteral
-            <|> fmap Var ident
-            <|> (reserved "true" >> return LitTrue)
-            <|> (reserved "false" >> return LitFalse)
+      , [ Infix (reservedOp "+" >> return (Add Plus)) AssocLeft
+        , Infix (reservedOp "-" >> return (Add Minus)) AssocLeft
+        ]
+      , [Prefix (reservedOp "!" >> return Not)]
+      , [ Infix (reservedOp "&&" >> return And) AssocLeft
+        , Infix (reservedOp "||" >> return Or) AssocLeft
+        ]
+      , [ Infix (reservedOp "<" >> return (Comp Less)) AssocNone
+        , Infix (reservedOp "<=" >> return (Comp LessEqual)) AssocNone
+        , Infix (reservedOp ">" >> return (Comp Greater)) AssocNone
+        , Infix (reservedOp ">=" >> return (Comp GreaterEqual)) AssocNone
+        , Infix (reservedOp "==" >> return (Comp Equal)) AssocNone
+        , Infix (reservedOp "!=" >> return (Comp NEqual)) AssocNone
+        ]
+      ]
+    terms =
+      parens expr <|> try functionCall <|> fmap LitInt integer <|>
+      fmap (LitString . T.pack) stringLiteral <|>
+      fmap Var ident <|>
+      (reserved "true" >> return LitTrue) <|>
+      (reserved "false" >> return LitFalse)
     functionCall = do
-        name <- ident
-        args <- parens (commaSep expr)
-        return $ Call name args
+      name <- ident
+      args <- parens (commaSep expr)
+      return $ Call name args
 
 ifStmt :: Parser Stmt
 ifStmt = do
-    reserved "if"
-    cond <- parens expr
-    If cond <$> stmt
+  reserved "if"
+  cond <- parens expr
+  If cond <$> stmt
 
 ifElseStmt :: Parser Stmt
 ifElseStmt = do
-    reserved "if"
-    cond <- parens expr
-    stm  <- stmt
-    reserved "else"
-    stm' <- stmt
-    IfElse cond stm <$> stmt
+  reserved "if"
+  cond <- parens expr
+  stm <- stmt
+  reserved "else"
+  stm' <- stmt
+  IfElse cond stm <$> stmt
 
 whileStmt :: Parser Stmt
 whileStmt = do
-    reserved "while"
-    cond <- parens expr
-    Loop cond <$> stmt
+  reserved "while"
+  cond <- parens expr
+  Loop cond <$> stmt
 
 assStmt :: Parser Stmt
 assStmt = do
-    var <- ident
-    reserved "="
-    Ass var <$> expr
-
+  var <- ident
+  reserved "="
+  Ass var <$> expr
 
 varDecl :: Parser DeclItem
 varDecl = withInit <|> noInit
   where
     withInit = do
-        name <- ident
-        reserved "="
-        DeclItem name . Just <$> expr
+      name <- ident
+      reserved "="
+      DeclItem name . Just <$> expr
     noInit = do
-        name <- ident
-        return $ DeclItem name Nothing
+      name <- ident
+      return $ DeclItem name Nothing
 
 declStmt :: Parser Stmt
 declStmt = do
-    t     <- typ
-    items <- commaSep varDecl
-    return $ Decl t []
+  t <- typ
+  items <- commaSep varDecl
+  return $ Decl t []
 
 typVar :: Parser TypVar
 typVar = do
-    t <- typ
-    TypVar t <$> ident
+  t <- typ
+  TypVar t <$> ident
 
 topDef :: Parser TopDef
 topDef = do
-    t     <- typ
-    name  <- ident
-    args  <- parens (commaSep typVar)
-    (Block stmts) <- blockStmt
-    return $ TopDef t name args stmts
+  t <- typ
+  name <- ident
+  args <- parens (commaSep typVar)
+  (Block stmts) <- blockStmt
+  return $ TopDef t name args stmts
 
 programParser :: Parser Program
 programParser = many topDef
