3f1272aac0967c48180763c820526888ce8fa2da
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2019-01-03 13:00:44 +0100

    Add arguement checking

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,29 @@ module Main where
 import qualified Parse                          as P
 import           System.Environment
 import           System.IO
+import           System.Exit
 
 import           Prettify
 import           Text.PrettyPrint.HughesPJClass
 
-main :: IO ()
-main = do
-  [filename] <- getArgs
-  putStrLn "Latte Compiler v0.1"
+
+failWithError :: String -> IO ()
+failWithError err = do
+    hPutStrLn stderr err
+    exitWith $ ExitFailure 1
+
+
+compileFile :: String -> IO ()
+compileFile filename = do
   res <- P.parseSource filename
   case res of
     Left err      -> print err
     Right program -> print $ pPrint program
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [filename] -> compileFile filename
+    _ -> failWithError "Invalid arguments!"
+--  putStrLn "Latte Compiler v0.1"
