acf992480922d2d14fcbbff85127a3862cc0bbff
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2019-01-20 16:18:37 +0100

    Add testing script

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for f in ./lattests/good/*.lat; do
+    OUT="$(mktemp)";
+    stack run $f > $OUT;
+    echo $f $?;
+    INFILE=${f%.*}.input;
+    OUTFILE=${f%.*}.output;
+    llvm-link $OUT ./lib/runtime.ll -o /tmp/output.bc;
+    if [ ! -f $INFILE ]; then
+        lli /tmp/output.bc > /tmp/output;
+    else
+        lli /tmp/output.bc < $INFILE > /tmp/output;
+    fi
+    diff /tmp/output $OUTFILE;
+done
+
+for f in ./lattests/bad/*.lat; do stack run $f > /dev/null; echo $f $?; done
